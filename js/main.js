f49a07939aa79ad9ebc39197a5c9cd9c855e8604
Kitty Tang <thepinkcodemonkey@gmail.com>
2013-03-21 02:36:12 -0700

    Added CSS
    Added helper function to construct search url,
    Wired up stop timer button
    Added simple validation

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ $(function(){
 	var TweetCollections = Backbone.Collection.extend({
 		model: TwitterFeed,
 		url: function () {
-		  return 'http://search.twitter.com/search.json?q=' + this.query + '&page=' + this.page + '&since_id='+this.sinceId+'&callback=?'
+		  return 'http://search.twitter.com/search.json?'+this.constructUri();
 		},
 
 		initialize: function() {
@@ -35,26 +35,47 @@ $(function(){
 			this.sinceId = '0';
 	    },
 
+	    constructUri: function()
+	    {
+	    	var uriParam = new Array();
+	    	uriParam.push('q='+escape(this.query));
+	    	uriParam.push('page='+this.page);
+	    	uriParam.push('since_id='+escape(this.sinceId));
+			uriParam.push('callback=?');
+			return uriParam.join('&');
+
+	    },
+
 		//overwrite the parse function to collect the result and catch errors
 		parse: function(resp, xhr) {
 
-			//process each tweet slightly to extrac the info we want
-			var processedResult = new Array();
-			var iteratorFunc = function(response){
-				var newObj = {
-					from_user: response.from_user,
-					profile_image_url: response.profile_image_url,
-					text: response.text,
-					created_at: response.created_at,
-					id: response.id
+			if(resp.error ===undefined)
+			{
+				//process each tweet slightly to extract the info we want
+				var processedResult = new Array();
+				var iteratorFunc = function(response){
+					var newObj = {
+						from_user: response.from_user,
+						profile_image_url: response.profile_image_url,
+						text: response.text,
+						created_at: response.created_at,
+						id: response.id
+					};
+					//insert the tweets in reverse order, so they can be populated in correct order
+					processedResult.unshift(newObj);
 				};
-				//insert the tweets in reverse order
-				processedResult.unshift(newObj);
-			};
-			this.sinceId = resp.max_id_str;
-			_.each(resp.results, iteratorFunc);
-			return(processedResult);
-			//return resp.results;
+
+				//update the since id, so we get the latest tweets
+				this.sinceId = resp.max_id_str;
+				_.each(resp.results, iteratorFunc);
+				return(processedResult);
+			}
+			else
+			{
+				//pass the error through an error event
+				this.trigger("Error", resp.error);
+				return([]);
+			}
 		},
 
 		//Sets all the parameters used
@@ -77,6 +98,7 @@ $(function(){
 		},
 	});
 	
+	//create an actual collection for tweets
 	var TwitterFeeds = new TweetCollections;
 
 	//main application, handles the search
@@ -87,23 +109,26 @@ $(function(){
 		// Bind events for search submission and manual refresh
 		events: {
 		  "click #submitButton": "submitSearch",
-		  "click #refreshButton": "refreshFeed"
+		  "click #refreshButton": "refreshFeed",
+		  "click #stopAutoRefresh": "stopTimer",
 		},
 
 		//Initialize the main application by loading last inputted search,
 		//biding shortcuts to elements and listening to the twitter feed collection
 		initialize: function()
 		{
-			$('#feed').hide();
 			this.currentSearch = '';
 
 			//Listening to events on TwitterFeeds Collection
 			this.listenTo(TwitterFeeds, "reset", this.addNewFeed);
+			this.listenTo(TwitterFeeds, "Error", this.displayNotification);
 
 			//binding shortcuts to DOM
 			this.searchInput = $('#searchInput');
 			this.refreshButton = $('#refreshButton');
 			this.searchOverview = $('#currentSearchString');
+			this.notificationPanel = $('#notificationPanel');
+			this.notification = $('#notification');
 
 			//initialize search string if it has been stored in local storage
 			this.currentSearch = localStorage.getItem('lastTwitterFeedSearch')||'';
@@ -112,27 +137,39 @@ $(function(){
 			//setup variables
 			this.intervalId = null;
 			this.intervalInSec = 5;
+
+			//set up view
+			$('#feed').hide();
+			this.notificationPanel.hide();
 		},
 
 		//call back to the reset event, adds a list of new feeds
 		addNewFeed: function(){
+			$('#feedList li').removeClass("newFeed");
 			TwitterFeeds.each(function(m){
 				var view = new TwitterFeedView({model: m});
+				view.$el.addClass("newFeed");
 				this.$("#feedList").prepend(view.render().el);
 			});
-			console.log('Add a lot of stuff');
 		},
 		
 		//Validates user search input and initiate the twitter search API call
 		//Resets the timer if the input is valid
 		submitSearch: function(){
-			//validate
-			this.currentSearch = this.searchInput.val();
-			$('#feed').show();
-			this.searchOverview.text("Currently Searching for '"+this.currentSearch+"'.");
-			//save search string to local storage
-			localStorage.setItem('lastTwitterFeedSearch', this.currentSearch);
-			this.refreshFeed();
+			//validate input
+			if(this.searchInput.val() === '')
+			{
+				this.displayNotification("Please enter some words to search for.");
+			}
+			else
+			{
+				$('#feed').show();
+				this.currentSearch = this.searchInput.val();
+				this.searchOverview.text("Currently Searching for '"+this.currentSearch+"'.");
+				//save search string to local storage
+				localStorage.setItem('lastTwitterFeedSearch', this.currentSearch);
+				this.refreshFeed();
+			}			
 		},
 
 		//Refreshes the search with the this.currentSearch param, resets the timer
@@ -143,13 +180,20 @@ $(function(){
 
 			TwitterFeeds.setSearchParam(this.currentSearch);
 			this.stopTimer();
+			fetchTweets();
 			this.intervalId = window.setInterval(fetchTweets, this.intervalInSec*1000);
-			console.log("Refreshing the feed "+this.currentSearch);
 		},
 
 		//stops the timer for auto fetching feeds
 		stopTimer: function(){
 			window.clearInterval(this.intervalId);
+		},
+
+		//display notifications if an error occurs and stops the auto refresh
+		displayNotification: function(message){
+			this.notification.text(message);
+			this.notificationPanel.show().fadeOut(2000);
+			this.stopTimer();
 		}
 	});
 
